80b7741775bf169d6e37465ef78e5573379d68a5
alain.ibrahim <alain.ibrahim@mnemotix.com>
2020-04-01 10:18:19 +0200

    fix bg

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-import Meteo from "./components/Meteo";
-import Categorie from "./components/Categorie";
+import Meteo from './components/Meteo';
+import Categorie from './components/Categorie';
 
 function App() {
   return (
@@ -10,9 +10,15 @@ function App() {
       <h5 style={{ paddingLeft: 15 }}>Météo</h5>
       <header className="App-header">
         <Meteo />
-        <Categorie name="air" display="Air" bottomOpacity />
-        <Categorie name="sol" display="Sol" bottomOpacity topOpacity />
-        <Categorie name="eau" display="Eau" topOpacity />
+        <Categorie name="air" display="Air" bgpos="bottom" bottomOpacity />
+        <Categorie
+          name="sol"
+          display="Sol"
+          bgpos="top"
+          bottomOpacity
+          topOpacity
+        />
+        <Categorie name="eau" display="Eau" bgpos="top" topOpacity />
       </header>
     </div>
   );
